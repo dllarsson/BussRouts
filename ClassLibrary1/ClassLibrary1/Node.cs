6a1004a8b37a628813f45db68f7a08f2510c7e36
Julia Call <54210280+Calljuly@users.noreply.github.com>
2020-04-21 15:03:10 +0200

    add
    Node and node connections

--- ClassLibrary1/ClassLibrary1/Node.cs
+++ ClassLibrary1/ClassLibrary1/Node.cs
@@ -1,8 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BussRout
 {
     public class Node
     {
+        public string Name { get; set; }
+        public List<string> start;
+        public List<Connections> Connections;
+        private Random randomWeight = new Random();
+        public Node(string name)
+        {
+            Name = name;
+            Connections = new List<Connections>();
+            start = new List<string>();
+            start.Add(name);
+        }
+        public void AddConnections(Node a, Node b)
+        {
+            int weight = GenerateRandomWeight();
+            Connections.Add(new Connections(a, b, weight));
+        }
+        public List<Connections> getConnections()
+        {
+            return Connections;
+        }
+        private int GenerateRandomWeight()
+        {
+            return randomWeight.Next(1, 10);
+        }
     }
 }
